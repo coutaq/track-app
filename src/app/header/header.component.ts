a2a84830ee4fba07b95d8c223f16811125bad88c
Coutaq <coutaq@gmail.com>
2022-07-23 05:31:26 +0200

    built app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TelegramLoginService } from '../telegram-login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  telegramLoginService:TelegramLoginService
+  constructor(telegramLoginService:TelegramLoginService) { 
+    this.telegramLoginService = telegramLoginService
+  }
 
   ngOnInit(): void {
   }
