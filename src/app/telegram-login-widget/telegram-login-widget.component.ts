67946d4f1fc0712c4051a61ca54c094b221f8c2f
Coutaq <coutaq@gmail.com>
2022-07-23 22:36:56 +0200

    built app

--- src/app/telegram-login-widget/telegram-login-widget.component.ts
+++ src/app/telegram-login-widget/telegram-login-widget.component.ts
@@ -17,7 +17,9 @@ export class TelegramLoginWidgetComponent implements AfterViewInit {
     script.setAttribute('data-telegram-login', 'testpd_bot');
     script.setAttribute('data-size', 'large');
     // Callback function in global scope
-    script.setAttribute('data-onauth', 'loginViaTelegram(user)');
+    // script.setAttribute('data-onauth', 'loginViaTelegram(user)');
+    script.setAttribute('data-auth-url', 'https://trackbot-mostalk.herokuapp.com/check')
+    // data-auth-url=""
     script.setAttribute('data-request-access', 'write');
     element.parentElement.replaceChild(script, element);
   }
